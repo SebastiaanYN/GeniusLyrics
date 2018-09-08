ddf6ec870133323645e65d6739f1693e26f58698
SebastiaanYN <sebastiaanyn@gmail.com>
2018-09-08 17:26:49 +0200

    Token will be read from a file instead of being hardcoded

--- src/com/sebastiaanyn/Main.java
+++ src/com/sebastiaanyn/Main.java
@@ -3,20 +3,28 @@ package com.sebastiaanyn;
 import com.sebastiaanyn.genius.Genius;
 import com.sebastiaanyn.genius.request.data.Search;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    private static final String TOKEN = "";
-    private static final Genius GENIUS = new Genius(TOKEN);
-
     public static void main(String[] args) {
+        String token = "";
+        try (final Scanner fileContent = new Scanner(new File("token.txt"))) {
+            token = fileContent.nextLine();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        Genius genius = new Genius(token);
+
         try (final Scanner in = new Scanner(System.in)) {
             System.out.print("Name a song: ");
             String query = in.nextLine();
-            Search search = GENIUS.search(query);
+            Search search = genius.search(query);
 
             List<Search.Response.Song> hits = search.response.hits.subList(0, 5);
             System.out.println("\nGot " + hits.size() + " possible hits");
@@ -29,7 +37,7 @@ public class Main {
             System.out.println("\n" + song.fullTitle + " - " + song.stats.pageviews + " views");
             System.out.println("Artist: " + song.primaryArtist.name + " - Verified: " + song.primaryArtist.isVerified);
 
-            String lyrics = GENIUS.getLyrics(song.url);
+            String lyrics = genius.getLyrics(song.url);
             System.out.println("\n" + lyrics);
         }
     }
